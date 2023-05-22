0fde2960f4919f145fbacb3c655988ba82834c56
kevinelong <kevinelong@gmail.com>
2023-05-22 09:47:31 -0700

    refactor

---
+++ scripts/arrows.js
@@ -0,0 +1,40 @@
+function arrowPad(){
+    const html = `
+    <table id="arrows">
+        <tr>
+            <td>
+                
+            </td>
+            <td>
+                <button id="up">^</button>
+            </td>
+            <td>
+                
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <button  id="left">&lt;</button>
+            </td>
+            <td>
+                
+            </td>
+            <td>
+                <button id="right">&gt;</button>                
+            </td>
+        </tr>
+        <tr>
+            <td>
+                
+            </td>
+            <td>
+                <button id="down">v</button>                                
+            </td>
+            <td>
+                
+            </td>
+        </tr>
+    </table>
+    `;
+    return html;
+}
