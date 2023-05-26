55dcf9ad5bb4fe0a5c2ab34d83e942fc196f8d5f
kevinelong <kevinelong@gmail.com>
2023-05-25 20:54:17 -0700

    refactor to views

---
+++ scripts/classes/models/DOMView.js
@@ -0,0 +1,64 @@
+class DOMView {
+  constructor(game) {
+    this.game = game;
+    document.body.innerHTML = world(score() + stats() + arrowPad() + map());
+
+    "map score stats up down left right".split(" ").forEach((cls) => {
+      window[cls] = document.querySelector(`.${cls}`);
+    });
+    get("up").addEventListener("click", this.arrowClick);
+    get("down").addEventListener("click", this.arrowClick);
+    get("left").addEventListener("click", this.arrowClick);
+    get("right").addEventListener("click", this.arrowClick);
+
+    document.addEventListener("keydown", (event) => {
+      console.log(event.keyCode);
+      let CODES = {
+        37: "left",
+        38: "up",
+        39: "right",
+        40: "down",
+        65: "left", //a
+        87: "up", //w
+        83: "down", //s
+        68: "right", //d
+      };
+      this.game.move(CODES[event.keyCode]);
+      this.render();
+    });
+  }
+  arrowClick(e) {
+    this.game.move(e.target.id);
+    this.render();
+  }
+  render() {
+    //DRAW
+    //window.map.innerHTML = ""; //clear the map
+    this.game.entities.forEach((sprite, i) => {
+      let div;
+      if (undefined === sprite.element) {
+        div = document.createElement("img");
+        div.src = "images/" + sprite.name + ".png";
+        //div.setAttribute("style", 'width:1rem;height:1rem;object-fit:contain;');
+        div.style.zIndex = (-i).toString();
+        
+        sprite.element = div;
+        window.map.appendChild(div); //append to map
+      } else {
+        div = sprite.element;
+      }
+      if (sprite.removed) {
+        div.classList.add("removed");
+      }
+      div.style.left = sprite.position.x + "rem";
+      div.style.top = sprite.position.y + "rem";
+    });
+    window.score.innerHTML = this.game.hero.score;
+
+    let output = "";
+    for (let life = 0; life < this.game.lives; life++) {
+      output += "@ ";
+    }
+    window.stats.innerHTML = output;
+  }
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,80 +1,9 @@
-let g = new Game();
+document.addEventListener("DOMContentLoaded", () => {
+  let g = new Game();
 
- document.addEventListener("DOMContentLoaded", () => {
-
-     document.body.innerHTML = world(score() + stats() + arrowPad() + map());
-
-     "map score stats up down left right".split(" ").forEach(cls => {
-          window[cls] = document.querySelector(`.${cls}`);
-      });
+  let view = new DOMView(g);
 
 
-	MODE = "images";
 
-     function render() { //DRAW
-         //window.map.innerHTML = ""; //clear the map
-         g.entities.forEach((sprite, i) => {
-             let div;
-             if(undefined === sprite.element){
-		     if ("images" === MODE) {
-			 div = document.createElement("img");
-			 div.src = "images/" + sprite.name + ".png";
-			 //div.setAttribute("style", 'width:1rem;height:1rem;object-fit:contain;');
-			 div.style.zIndex = (-i).toString();
-		     } else {
-			 div = document.createElement("div");
-			 div.innerHTML = sprite.symbol;
-		     }
-                     sprite.element = div;
-                     window.map.appendChild(div); //append to map
-             }else{
-		div=sprite.element;
-	     }
-             if(sprite.removed){
-		div.classList.add("removed");
-             }
-             div.style.left = sprite.position.x + "rem";
-             div.style.top = sprite.position.y + "rem";
-         })
-         window.score.innerHTML = g.hero.score;
-
-         let output = "";
-         for (let life = 0; life < g.lives; life++) {
-             output += "@ ";
-         }
-         window.stats.innerHTML = output;
-
-     }
-
-     function arrowClick(e) {
-         let id = e.target.id;
-	 g.move(id)
-         render();
-     }
-
-     get("up").addEventListener("click", arrowClick)
-     get("down").addEventListener("click", arrowClick)
-     get("left").addEventListener("click", arrowClick)
-     get("right").addEventListener("click", arrowClick)
-
-	document.addEventListener("keydown",(event) => {
-		console.log(event.keyCode)
-		let CODES = {
-			37: "left",
-			38: "up",
-			39: "right",
-			40: "down",
-			65: "left", //a
-			87: "up",   //w
-			83: "down", //s
-			68: "right",//d
-		};
-		g.move(CODES[event.keyCode]);
-		render();
-	});
-
-
-     render()
-
-
- });//end DCL
+  view.render();
+}); //end DCL
